daa9f4bcb4c9ed23be7cf1084a59b1ef9c90b66e
NOOBIERAM <rotsiniainagg@gmail.com>
2025-12-18 23:35:46 +0300

    ep

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/style.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import {  HashRouter } from 'react-router-dom'
 import { store } from './store'
 import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>    
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <HashRouter>
+  <App />
+</HashRouter>
     </Provider>
   </StrictMode>,
 )
