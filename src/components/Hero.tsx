853619111c9d75d4c7eca1e48bc871aeec3beac0
NOOBIERAM <rotsiniainagg@gmail.com>
2025-12-20 22:22:13 +0300

    re design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import { Github, ChevronRight, TerminalSquareIcon, Linkedin, Facebook,  CornerDownLeftIcon, Mail, Phone, MapPin,  CircleChevronRight, CircleChevronLeft } from "lucide-react";
 import React from "react";
+import AsciiLogo from "./AsciiLogo";
 
 
 const Hero: React.FC = () => {
@@ -26,10 +27,10 @@ const Hero: React.FC = () => {
             <div className="max-w-7xl mx-auto translate-y-12 h-150">
                 <div className="grid md:grid-cols-2 gap-12 items-center h-full">
                     <div className="">
-                        <h2 className=" text-5xl  py-4 mb-4  leading-tight rounded-2xl">
-                            👋 Hello, I'm <span className="font-bold">Rotsi.</span>
+                        <h2 className=" text-5xl  py-4 mb-4  leading-tight rounded-2xl ">
+                            👋 Hello, <span className="text-gray-300">I'm <span className="font-bold">Rotsi.</span></span>
                         </h2>
-                        <p className="text-xl md:text-lg  text-white/90 mb-30 leading-relaxed font-normal drop-shadow-[0_1px_6px_rgba(0,0,0,0.3)]">
+                        <p className="text-xl md:text-lg  mb-30 leading-relaxed font-normal drop-shadow-[0_1px_6px_rgba(0,0,0,0.3)]">
                             A developer pasionate about creating innovative, efficient and scalable digital solutions. I combine creativity and technical expertise to bring ideas to life in the digital world.
                         </p>
                         <div className="flex justify-start animate-fade-in-up delay-400 -translate-y-12 space-x-4 ">
@@ -41,9 +42,9 @@ const Hero: React.FC = () => {
                                 <span>Enter</span> <CornerDownLeftIcon /></a>
                         </div>
                     </div>
-                    <div className="max-w-7xl mx-auto border-2  rounded-2xl w-full bg-teal-900/20">
-                        <div className="flex items-center justify-between border-b-2 p-2 px-3 font-bold">
-                            <span className="flex items-center"><TerminalSquareIcon className="me-3" />Hello world_</span>
+                    <div className="max-w-7xl mx-auto border-2 border-gray-700 rounded-2xl w-full bg-black/30 text-gray-500">
+                        <div className="flex items-center justify-between border-b-2 border-gray-700 p-2 px-3 font-bold">
+                            <span className="flex items-center"><TerminalSquareIcon className="me-3 text-sm text-gray-300" />Hello world_</span>
                             <div className="flex items-center space-x-2">
                                 <span className="w-4 h-4 bg-teal-500/10 rounded-full"></span>
                                 <span className="w-4 h-4 bg-teal-500/20 rounded-full"></span>
@@ -54,34 +55,56 @@ const Hero: React.FC = () => {
                                 <a href="#" className="flex border rounded-full px-3 p-1 text-sm"><Download size={20} className="me-3" />CV</a>
                             </div> */}
                         </div>
-                        <div className="p-5 font-mono text-lg text-white">
-                            <div className="mb-6 border-r-3 flex flex-col">
-                                <span className="flex items-center mb-2 text-teal-300"><ChevronRight size={18} />$ whoami</span>
-                                <span className="ms-6 font-extrabold">RAMANANTSOA Rotsiniaina</span>
-                                <span className="ms-6 text-sm flex items-center "># Fullstack Developer </span>
+                        <div className="p-5 font-mono text-lg text-gray-400">
+                            <div className="mb-6 flex flex-col">
+                                <span className="ms-6 mb-5">
+                                    <AsciiLogo/>
+                                </span>
+                                <div className="flex items-center justify-center space-x-3 mb-5">
+                                    <span className="flex items-center font-mono"><CircleChevronLeft size={15} />──────</span>
+                                        <a href="https://github.com/NOOBIERAM" target="_blank"
+                                            rel="noopener noreferrer" className="flex items-center text-teal-600 hover:text-teal-500 border p-1 rounded bg-gray-900"><Github size={18} /></a>
+                                        <span className="font-mono">────</span>
+                                        <a href="#" target="_blank"
+                                            rel="noopener noreferrer" className="flex items-center text-teal-600 hover:text-teal-500 border p-1 rounded bg-gray-900"><Linkedin size={18} /></a>
+                                        <span className="font-mono">────</span>
+                                        <a href="#" target="_blank"
+                                            rel="noopener noreferrer" className="flex items-center text-teal-600 hover:text-teal-500 border p-1 rounded bg-gray-900"><Facebook size={18} /></a>
+                                        <span className="font-mono">────</span>
+                                        <a href="#" target="_blank"
+                                            rel="noopener noreferrer" className="flex items-center text-teal-600 hover:text-teal-500 text-sm font-bold border p-1 rounded bg-gray-900">CV</a>                                        
+                                        <span className="flex items-center font-mono">──────<CircleChevronRight size={15} /></span>
+                                </div>
+                               
 
                             </div>
-                            <div className="mb-6 border-r-3">
-                                <span className="flex items-center mb-2 text-teal-300"><ChevronRight size={18} /> $ cat ~/Contact & Network</span>
-                                <ul className="text-sm mt-4 space-y-1 ms-6">
+                            <div className="mb-6 border-r-3 border-gray-700 flex flex-col">
+                                 <span className="flex items-center mb-2 text-teal-500 text-sm"><ChevronRight size={18} />$ whoami</span>
+                                
+
+                                <span className="ms-6 text-[15px] flex items-center justify-center"># Fullstack Developer </span>
+                            </div>
+                            <div className="mb-6 border-r-3 border-gray-700">
+                                <span className="flex items-center mb-2 text-teal-500 text-sm"><ChevronRight size={18} /> $ cat ~/Contact & Network</span>
+                                <ul className="text-[15px] mt-4 space-y-1 ms-6 flex flex-col items-center justify-center">
                                     <li>==============================================</li>
-                                    <li className="text-sm ms-6  flex items-center"><Mail size={15} className="me-3" /> rotsiniainaramanantsoa.gg@gmail.com </li>
-                                    <li className="text-sm ms-6  flex items-center"><Phone size={15} className="me-3" /> +261 34 77 006 26 </li>
-                                    <li className="text-sm ms-6  flex items-center"><MapPin size={15} className="me-3" /> Madagascar</li>
+                                    <li className="text-[15px] ms-6  flex items-center"><Mail size={15} className="me-3" /> rotsiniainaramanantsoa.gg@gmail.com </li>
+                                    <li className="text-[15px] ms-6  flex items-center"><Phone size={15} className="me-3" /> +261 34 77 006 26 </li>
+                                    <li className="text-[15px] ms-6  flex items-center"><MapPin size={15} className="me-3" /> Madagascar</li>
                                     <li>==============================================</li>
-                                    <li className="flex items-center space-x-3">
+                                    {/* <li className="flex items-center space-x-3">
                                         <span className="flex items-center"><CircleChevronLeft size={15} />----</span>
                                         <a href="https://github.com/NOOBIERAM" target="_blank"
-                                            rel="noopener noreferrer" className="flex items-center hover:text-lg text-teal-500 hover:text-teal-700"><Github size={18} /></a>
+                                            rel="noopener noreferrer" className="flex items-center hover:text-lg text-teal-500 hover:text-teal-700 border p-1 rounded bg-white"><Github size={18} /></a>
                                         <span>----</span>
                                         <a href="#" target="_blank"
-                                            rel="noopener noreferrer" className="flex items-center hover:text-lg text-teal-500 hover:text-teal-700"><Linkedin size={18} /></a>
+                                            rel="noopener noreferrer" className="flex items-center hover:text-lg text-teal-500 hover:text-teal-700 border p-1 rounded bg-white"><Linkedin size={18} /></a>
                                         <span>----</span>
                                         <a href="#" target="_blank"
-                                            rel="noopener noreferrer" className="flex items-center hover:text-lg text-teal-500 hover:text-teal-700"><Facebook size={18} /></a>
+                                            rel="noopener noreferrer" className="flex items-center hover:text-lg text-teal-500 hover:text-teal-700 border p-1 rounded bg-white"><Facebook size={18} /></a>
                                         <span className="flex items-center">----<CircleChevronRight size={15} /></span>
 
-                                    </li>
+                                    </li> */}
                                     {/* <li><a href="#" target="_blank"
                                         rel="noopener noreferrer" className="flex items-center hover:text-teal-600 hover:underline">----<Linkedin size={18} className="mx-3" /> Linkedin</a></li>
                                     <li><a href="#" target="_blank"
@@ -91,7 +114,7 @@ const Hero: React.FC = () => {
 
                                 </ul>
                             </div>
-                            <div className="mb-6 border-r-3">
+                            <div className="mb-6 border-r-3 border-gray-700 text-sm">
                                 <span className="flex items-center mb-2"><ChevronRight size={18} /><span className="animate-pulse duration-75 font-extrabold">_</span></span>
                             </div>
                         </div>
